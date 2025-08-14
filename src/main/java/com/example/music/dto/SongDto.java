90fd7b638a97fab427bbb4d2ac3163e235c197ba
chris Taylor <ctay456a@gmail.com>
2025-08-14 16:45:37 -0400

    Create SongDto.java

---
+++ src/main/java/com/example/music/dto/SongDto.java
@@ -0,0 +1,11 @@
+package com.example.music.dto;
+
+public class SongDto {
+    public Long id;
+    public String title;
+    public Integer durationSeconds;
+    public String mimeType;
+    public String filePath;
+    public Long albumId;
+    public Long artistId;
+}
